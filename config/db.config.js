af012ce111144c403419ec8b0b52fb6a9ba23987
Luis Osorio <losorio@consejodeauditoria.gob.cl>
2020-02-03 14:57:19 -0300

    test

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "limatco"
+    HOST: "db4free.net",
+    USER: "leonidas",
+    PASSWORD: "leonidas123",
+    DB: "limatcotest"
   };
+
+  // HOST: "localhost",
+  //   USER: "root",
+  //   PASSWORD: "",
+  //   DB: "limatco"
--- models/db.js
+++ models/db.js
@@ -11,8 +11,13 @@ const connection = mysql.createConnection({
 
 // open the MySQL connection
 connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
+  if (error){
+      console.log("Error conectar base de datos.", error.message);
+  }else{
+    
+    console.log("Successfully connected to the database.");
+  }
+
 });
 
 module.exports = connection;
